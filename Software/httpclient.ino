362701ffb4e9d2adf472de51e27ccbce830af6d9
Marco Adrián Guido <mguido@ndscognitivelabs.com>
2020-03-01 02:52:34 -0600

    added esp33 http wifi client json parser

--- Software/httpclient.ino
+++ Software/httpclient.ino
@@ -1,92 +1,71 @@
-// It performs a GET resquest on arduinojson.org/example.json
-// Here is the expected response:
-// {
-//   "sensor": "gps",
-//   "time": 1351824120,
-//   "data": [
-//     48.756080,
-//     2.302038
-//   ]
-// }
-//
-// https://arduinojson.org/v6/example/http-client/
-
+//si usas esp32
+#include <HTTPClient.h>
+#include <WiFi.h>
 #include <ArduinoJson.h>
-#include <Ethernet.h>
-#include <SPI.h>
+//si usas esp8266
+//#include <ESP8266WiFiMulti.h>
+//#include <ESP8266HTTPClient.h>
+
+const char* ssid = "INFINITUM9576_2.4";
+const char* password =  "ht6pwZnQAv";
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) continue;
+  delay(10);
+  Serial.begin(115200);
 
-  byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-  if (!Ethernet.begin(mac)) {
-    Serial.println(F("Failed to configure Ethernet"));
-    return;
-  }
-  delay(1000);
 
-  Serial.println(F("Connecting..."));
+  WiFi.begin(ssid, password);
 
-  EthernetClient client;
-  client.setTimeout(10000);
-  if (!client.connect("arduinojson.org", 80)) {
-    Serial.println(F("Connection failed"));
-    return;
+  Serial.print("Conectando...");
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println(F("Connected!"));
+  Serial.print("Conectado con éxito, mi IP es: ");
+  Serial.println(WiFi.localIP());
 
-  // Send HTTP request
-  client.println(F("GET /example.json HTTP/1.0"));
-  client.println(F("Host: arduinojson.org"));
-  client.println(F("Connection: close"));
-  if (client.println() == 0) {
-    Serial.println(F("Failed to send request"));
-    return;
-  }
-
-  // Check HTTP status
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  // It should be "HTTP/1.0 200 OK" or "HTTP/1.1 200 OK"
-  if (strcmp(status + 9, "200 OK") != 0) {
-    Serial.print(F("Unexpected response: "));
-    Serial.println(status);
-    return;
-  }
-
-  // Skip HTTP headers
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders)) {
-    Serial.println(F("Invalid response"));
-    return;
-  }
-
-  // Allocate the JSON document
-  // Use arduinojson.org/v6/assistant to compute the capacity.
-  const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 60;
-  DynamicJsonDocument doc(capacity);
-
-  // Parse JSON object
-  DeserializationError error = deserializeJson(doc, client);
-  if (error) {
-    Serial.print(F("deserializeJson() failed: "));
-    Serial.println(error.c_str());
-    return;
-  }
-
-  // Extract values
-  Serial.println(F("Response:"));
-  Serial.println(doc["sensor"].as<char*>());
-  Serial.println(doc["time"].as<long>());
-  Serial.println(doc["data"][0].as<float>(), 6);
-  Serial.println(doc["data"][1].as<float>(), 6);
-
-  // Disconnect
-  client.stop();
 }
 
 void loop() {
-  // not used in this example
+
+  if(WiFi.status()== WL_CONNECTED){   //Check WiFi connection status
+
+    HTTPClient http;
+    String datos_a_enviar = "{\"data\" : 410}";
+
+    http.begin("http://192.168.1.129:5000/");        //Indicamos el destino
+    http.addHeader("Content-Type", "application/json"); //Preparamos el header json
+    //int codigo_respuesta = http.POST(datos_a_enviar);   //Enviamos el post pasándole, los datos que queremos enviar. (esta función nos devuelve un código que guardamos en un int)
+
+
+    DynamicJsonDocument doc(200); // <- 200 bytes in the heap
+
+    doc["accelX"] = 0;
+    doc["accelY"] = 50;
+    doc["accelZ"] = 100;
+    doc["status"] = "world";
+    doc["line"] = 0.0;
+
+    String output = doc.as<String>();
+    Serial.println(output);
+    int codigo_respuesta = http.POST(output);   //Enviamos el post pasándole, los datos que queremos enviar. (esta función nos devuelve un código que guardamos en un int)
+
+    if(codigo_respuesta>0){
+      Serial.println("Código HTTP: " + String(codigo_respuesta));   //Print return code
+      if(codigo_respuesta == 200){
+        String cuerpo_respuesta = http.getString();
+        Serial.println("El servidor respondió:");
+        Serial.println(cuerpo_respuesta);
+      }
+    }else{
+     Serial.print("Error enviando POST, código: ");
+     Serial.println(codigo_respuesta);
+    }
+    http.end();  //libero recursos
+  }else{
+     Serial.println("Error en la conexión WIFI");
+  }
+
+   delay(2000);
 }
